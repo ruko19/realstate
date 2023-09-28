207503bd8657d665ea2299810a94ad76a5359346
ruko19 <rukobaruna23@gmail.com>
2023-09-27 20:23:35 -0500

    feat: frontend template setup part 2

---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,68 @@
+
+<x-front-layout>
+
+    <!-- banner-section -->
+    <x-home.banner-section />
+    <!-- banner-section end -->
+
+
+    <!-- category-section -->
+    <x-home.category />
+
+    <!-- category-section end -->
+
+
+    <!-- feature-section -->
+   <x-home.features />
+    <!-- feature-section end -->
+
+
+    <!-- video-section -->
+  <x-home.video-section />
+    <!-- video-section end -->
+
+
+    <!-- deals-section -->
+  <x-home.deals />
+    <!-- deals-section end -->
+
+
+    <!-- testimonial-section end -->
+    <x-home.testimonial />
+    <!-- testimonial-section end -->
+
+
+    <!-- chooseus-section -->
+    <x-home.chooseus />
+    <!-- chooseus-section end -->
+
+
+    <!-- place-section -->
+    <x-home.place />
+    <!-- place-section end -->
+
+
+    <!-- team-section -->
+    <x-home.team />
+    <!-- team-section end -->
+
+
+    <!-- cta-section -->
+    <x-home.cta />
+    <!-- cta-section end -->
+
+
+    <!-- news-section -->
+    <x-home.news />
+    <!-- news-section end -->
+
+
+    <!-- download-section -->
+    <x-home.download />
+    <!-- download-section end -->
+
+
+
+
+
+</x-front-layout>
